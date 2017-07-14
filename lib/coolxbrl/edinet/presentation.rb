979c4cfd3ac6254f1860d0af25b77acbd37db352
Yuki Yaegashi <xxx.y.a.e.xxx@me.com>
2017-07-15 00:07:29 +0900

    修正

--- lib/coolxbrl/edinet/presentation.rb
+++ lib/coolxbrl/edinet/presentation.rb
@@ -23,9 +23,9 @@ module CoolXBRL
             table.xpath("link:presentationArc").each do |arc|
               parent_name = arc.at_xpath("@xlink:from").to_s
               child_name  = arc.at_xpath("@xlink:to").to_s
-              puts table.xpath("link:loc[@xlink:label='#{parent_name}']").to_s
-              Node.new(parent:          { name: parent_name, locator: table.xpath("link:loc[@xlink:label='#{parent_name}']").to_s },
-                       child:           { name: child_name, locator: table.xpath("link:loc[@xlink:label='#{child_name}']").to_s },
+              puts table.xpath("link:loc[@xlink:label='#{parent_name}']/@xlink:label").to_s
+              Node.new(parent:          { name: parent_name, locator: table.xpath("link:loc[@xlink:label='#{parent_name}']/@xlink:label").to_s },
+                       child:           { name: child_name, locator: table.xpath("link:loc[@xlink:label='#{child_name}']/@xlink:label").to_s },
                        order:           arc.at_xpath("@order").to_s,
                        preferred_label: arc.at_xpath("@preferredLabel").to_s)
             end
