b327dff9f4144a49f1ed0fd10e678057be539a8d
Yuki Yaegashi <xxx.y.a.e.xxx@me.com>
2017-07-17 12:44:05 +0900

    修正

--- lib/coolxbrl/edinet.rb
+++ lib/coolxbrl/edinet.rb
@@ -5,22 +5,27 @@ require "coolxbrl/edinet/statement_of_comprehensive_income"
 require "coolxbrl/edinet/statement_of_shareholders_equity"
 require "coolxbrl/edinet/document_information"
 require "coolxbrl/edinet/presentation"
+require "coolxbrl/edinet/xsd"
 
 module CoolXBRL
   module EDINET
+    include XSD
+
     class << self
       def parse(dir)
         #Dir.chdir(dir)
         #doc = File.open(Dir.glob("XBRL/PublicDoc/**/*.xbrl").first) {|file| Nokogiri::XML file }
         #read doc
-        Presentation.parse dir
+        get_taxonomy dir
+        return @@xsd, @@presentation, @@label, @@label_en, @@xbrl
+        #Presentation.parse dir
 
       end
 
-      def get_file(dir, pattern)
-        Dir.chdir(dir)
-        File.open(Dir.glob("XBRL/PublicDoc/**/#{pattern}").first) {|file| Nokogiri::XML file }
-      end
+#      def get_file(dir, pattern)
+#        Dir.chdir(dir)
+#        File.open(Dir.glob("XBRL/PublicDoc/**/#{pattern}").first) {|file| Nokogiri::XML file }
+#      end
 
       private
       def read(doc)
