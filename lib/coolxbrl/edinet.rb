a8cf7bf2e7e34749548a90747c08dc2b3760c16b
Yuki Yaegashi <xxx.y.a.e.xxx@me.com>
2017-07-17 13:17:58 +0900

    修正

--- lib/coolxbrl/edinet.rb
+++ lib/coolxbrl/edinet.rb
@@ -17,7 +17,7 @@ module CoolXBRL
         #doc = File.open(Dir.glob("XBRL/PublicDoc/**/*.xbrl").first) {|file| Nokogiri::XML file }
         #read doc
         get_taxonomy dir
-        return label
+        return label.keys
         #Presentation.parse dir
 
       end
