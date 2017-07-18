ca952531e7a9fbabbd904757400e3ce0f0a64233
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-07-18 10:46:04 +0900

    修正

--- lib/coolxbrl/edinet/label.rb
+++ lib/coolxbrl/edinet/label.rb
@@ -5,6 +5,8 @@ module Label
     label_files = english_flag ? CoolXBRL::EDINET.label_en : CoolXBRL::EDINET.label
     label_files.each do |name, doc|
       if /http\:\/\/disclosure\.edinet\-fsa\.go\.jp\/taxonomy\/jppfs\// =~ name
+        puts locator
+        puts name
         puts Pathname(locator).relative_path_from(Pathname(name)).to_s
         puts doc.xpath("//link:loc[1]/@xlink:href").to_s
       else
