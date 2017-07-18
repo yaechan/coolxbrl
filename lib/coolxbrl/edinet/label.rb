6d988ad9aa3227d1b8ce8aff7b4ed04f6a3397a6
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-07-18 15:18:37 +0900

    修正

--- lib/coolxbrl/edinet/label.rb
+++ lib/coolxbrl/edinet/label.rb
@@ -4,17 +4,20 @@ module Label
 
   def get_label(locator, preferred_label, english_flag=false)
     label_files = english_flag ? CoolXBRL::EDINET.label_en : CoolXBRL::EDINET.label
+    role = preferred_label || STANDARD_LABEL
+
     if /http\:\/\// =~ locator
       name, doc = label_files[:edinet]
-      role = preferred_label || STANDARD_LABEL
-      locator_label = doc.at_xpath("//link:loc[@xlink:href='../#{File.basename(locator)}']/@xlink:label").to_s
-      doc.xpath("//link:labelArc[@xlink:from='#{locator_label}']/@xlink:to").each do |to|
-        label = doc.at_xpath("//link:label[@xlink:label='#{to.to_s}' and @xlink:role='#{role}']/text()").to_s
-        break label unless label.empty?
-      end
+      href = "../#{File.basename(locator)}"
     else
       name, doc = label_files[:presenter]
-      nil
+      href = locator
+    end
+
+    locator_label = doc.at_xpath("//link:loc[@xlink:href='#{href}']/@xlink:label").to_s
+    doc.xpath("//link:labelArc[@xlink:from='#{locator_label}']/@xlink:to").each do |to|
+      label = doc.at_xpath("//link:label[@xlink:label='#{to.to_s}' and @xlink:role='#{role}']/text()").to_s
+      break label unless label.empty?
     end
   end
 end
