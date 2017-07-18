9ee109839edce5ec00fa1a209025c92c262cf5c0
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-07-18 16:03:27 +0900

    修正

--- lib/coolxbrl/edinet/label.rb
+++ lib/coolxbrl/edinet/label.rb
@@ -3,24 +3,17 @@ module Label
   VERBOSE_LABEL  = "http://www.xbrl.org/2003/role/verboseLabel"
 
   def get_label(locator, preferred_label, english_flag=false)
-#    label_files = english_flag ? CoolXBRL::EDINET.label_en : CoolXBRL::EDINET.label
-#    role = preferred_label || STANDARD_LABEL
-#
-#    if /http\:\/\// =~ locator
-#      name, doc = label_files[:edinet]
-#      href = "../#{File.basename(locator)}"
-#    else
-#      name, doc = label_files[:presenter]
-#      href = locator
-#    end
     name, doc, href, role = create_params(locator, preferred_label, english_flag)
-
+puts doc.methods
+exit 0
     locator_label = doc.at_xpath("//link:loc[@xlink:href='#{href}']/@xlink:label").to_s
-    doc.xpath("//link:label[contains(./@xlink:label, '#{locator_label}') and @xlink:role='#{role}']/text()").to_s
+    #確実な検索。時間が掛かる
     #doc.xpath("//link:labelArc[@xlink:from='#{locator_label}']/@xlink:to").each do |to|
     #  label = doc.at_xpath("//link:label[@xlink:label='#{to.to_s}' and @xlink:role='#{role}']/text()").to_s
     #  break label unless label.empty?
     #end
+    #不確実かもしれない検索。多少時間が速くなる
+    doc.xpath("//link:label[contains(./@xlink:label, '#{locator_label}') and @xlink:role='#{role}']/text()").to_s
   end
 
   def create_params(locator, preferred_label, english_flag)
