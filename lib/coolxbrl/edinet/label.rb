417c19e0c94e9463b7a80492e512dfa047db77ee
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-07-18 10:58:48 +0900

    修正

--- lib/coolxbrl/edinet/label.rb
+++ lib/coolxbrl/edinet/label.rb
@@ -1,14 +1,10 @@
-require "pathname"
-
 module Label
   def get_label(locator, english_flag=false)
     label_files = english_flag ? CoolXBRL::EDINET.label_en : CoolXBRL::EDINET.label
     label_files.each do |name, doc|
       if /http\:\/\/disclosure\.edinet\-fsa\.go\.jp\/taxonomy\/jppfs\// =~ name
-        puts locator
+        puts File.basename(locator)
         puts name
-        puts Pathname(locator).relative_path_from(Pathname(name)).to_s
-        puts Pathname.new(File.join(name, doc.xpath("//link:loc[1]/@xlink:href").to_s).to_s).realpath
       else
       end
     end
