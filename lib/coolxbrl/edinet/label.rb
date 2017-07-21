bc2cea26045fb86eb443c0519372088cf265ea7f
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-07-21 14:37:42 +0900

    修正

--- lib/coolxbrl/edinet/label.rb
+++ lib/coolxbrl/edinet/label.rb
@@ -10,7 +10,7 @@ module CoolXBRL
         end
 
         def get_context_label(context, english_flag=false)
-          search_label(*create_context_params(context, english_flag))
+          search_context_label(*create_context_params(context, english_flag))
         end
 
         def create_params(locator, preferred_label, english_flag)
@@ -42,7 +42,6 @@ module CoolXBRL
           #  break label unless label.empty?
           #end
           #不確実かもしれない検索。多少時間が速くなる
-          puts context
           doc.xpath("//link:label[contains(./@xlink:label, '#{context}') and @xlink:role='#{role}']/text()").to_s
         end
 
