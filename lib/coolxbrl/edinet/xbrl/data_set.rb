672321fe8305ebf06d17510020a08ca28bc022eb
Yuki Yaegashi <yaegashi@indb.co.jp>
2017-08-02 16:31:11 +0900

    修正

--- lib/coolxbrl/edinet/xbrl/data_set.rb
+++ lib/coolxbrl/edinet/xbrl/data_set.rb
@@ -7,7 +7,7 @@ module CoolXBRL
         def to_hash
           self.each_with_object({}) do |data, stack|
             context_ref = data.context_ref.scan(/(?<=Instant\_|Duration\_).+Member$/)[0]
-            #context_ref = data.context_ref
+
             stack[context_ref] = { :label => data.context_label, :data => [] } unless stack.has_key?(context_ref)
             stack[context_ref][:data] << { :period => data.period, :value => data.value }
             stack[context_ref][:data].sort_by!{|data| data[:period] }
@@ -17,10 +17,6 @@ module CoolXBRL
         def has_context_ref?(context_ref)
           self.find {|data| data.context_ref == context_ref }
         end
-
-        #def sort
-        #  self.sort_by{|data| data[:period] }
-        #end
       end
     end
   end
